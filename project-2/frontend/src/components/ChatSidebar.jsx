cbf801cd2084ca03791960a6ed0fb4632d38f4f3
sulyman560 <sulyman560@gmail.com>
2026-03-21 23:06:19 +0600

    fix

--- project-2/frontend/src/components/ChatSidebar.jsx
+++ project-2/frontend/src/components/ChatSidebar.jsx
@@ -9,17 +9,29 @@ const ChatSidebar = ({ users, selectUser, activeUser, userStatus }) => {
           <div
             key={u._id}
             onClick={() => selectUser(u)}
-            className=  {`flex justify-between items-center p-2 rounded cursor-pointer ${activeUser?._id === u._id ? "bg-gray-300" : "hover:bg-gray-300"}`}
+            className=  {`flex justify-between items-center border m-1 p-2 rounded cursor-pointer ${activeUser?._id === u._id ? "bg-gray-300" : "hover:bg-gray-300"}`}
           >
-            <span>{u.username}</span>
+            <span>
+              {
+                status?.online ? (
+                  <span>{u.username} <span className="text-xs text-green-600">Active now</span></span>
+                ) : (
+                  <span>{u.username} <span className="text-xs text-gray-600">Offline</span></span>
+                )
+              }
+            </span>
 
             <span>
               {status?.online ? (
-                <span className="w-3 h-3 bg-green-500 rounded-full inline-block"></span>
+                <span className="w-2 h-2 bg-green-600 border border-green-700 rounded-full inline-block"></span>
               ) : status?.lastSeen ? (
+                <>
+                <br />
                 <small className="text-red-500 text-xs">
                   {Math.floor((new Date() - new Date(status.lastSeen)) / 60000)} min ago
                 </small>
+                </>
+                
               ) : null}
             </span>
           </div>
