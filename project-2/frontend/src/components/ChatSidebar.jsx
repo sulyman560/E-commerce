5ad7049954a3f3d77c272c72acbdd2e4430d00ce
sulyman560 <sulyman560@gmail.com>
2026-03-22 03:05:20 +0600

    fix

--- project-2/frontend/src/components/ChatSidebar.jsx
+++ project-2/frontend/src/components/ChatSidebar.jsx
@@ -6,7 +6,7 @@ import Loading from "./Loading";
 const ChatSidebar = ({ loading, setLoading, sidebarOpen, setSidebarOpen, users, selectUser, activeUser, userStatus }) => {
   const { user, logout, Navigate } = useContext(AuthContext);
   return (
-    <div className={`bg-white w-80 xl:w-90 p-4 border-r border-gray-200 flex flex-col justify-between items-center 
+    <div className={`bg-white w-70 xl:w-80 p-4 border-r border-gray-200 flex flex-col justify-between items-center 
     max-sm:absolute top-0 bottom-0 z-20 ${sidebarOpen ? 'translate-x-0' : 'max-sm:-translate-x-full'} 
     transition-all duration-300 ease-in-out`}>
       <div className="w-full flex flex-col ">
